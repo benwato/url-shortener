edb55bf2095528216fb44211cdc5285a64d54e6e
benwato <benwato12@gmail.com>
2021-11-10 15:17:01 +0000

    update

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, flash
 from flask_cors import CORS
 from url_generator import generator
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 CORS(app)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
@@ -18,21 +19,28 @@ def home():
     if request.method == 'POST':
         print(request.form)
         url = request.form['url-input']
-        random = generator.generator()
-        new_url = f'http://localhost:5000/{random}'
-        post = Url(url=url, shortened=new_url)
-        db.session.add(post)
-        db.session.commit()
-        return render_template('form.html', url=url, shortened= new_url)
+        url_check = Url.query.filter(Url.url == url).first()
+        if url_check:
+            return render_template('form.html', url=url, shortened= url_check.shortened)
+        else:
+            random = generator.generator()
+            new_url = f'http://localhost:5000/{random}'
+            post = Url(url=url, shortened=new_url)
+            db.session.add(post)
+            db.session.commit()
+            return render_template('form.html', url=url, shortened= new_url)
     else:
         return render_template('form.html')
 
 @app.route('/<created_url>')
 def getUrl(created_url):
     check = f'http://localhost:5000/{created_url}'
-    url_check = Url.query.filter(Url.shortened == check)
-    print(url_check)
-    return redirect(url_check.url)
+    url_check = Url.query.filter(Url.shortened == check).first()
+    if url_check:
+        return redirect(url_check.url)
+    else:
+        flash("Ooops, we don't seem to have that address")
+        return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
